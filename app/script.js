49339beea8996ca544ecab3eae26adeea963d3c2
Diego Desktop <disouzauva@gmail.com>
2021-02-09 23:11:31 -0300

    Centralizando o canvas

--- app/script.js
+++ app/script.js
@@ -1,1 +1,14 @@
-alert('Hello world');
+var canvas = document.querySelector('canvas');
+var brush = canvas.getContext('2d');
+
+criaRetangulo(black,0,0,600,400);
+
+
+
+
+// Funções de formas
+
+function criaRetangulo(cor,xInicial,yInicial,xFinal,yFinal) {
+    brush.fillStyle = cor;
+    brush.fillRect(xInicial,yInicial,xFinal,yFinal);
+}
