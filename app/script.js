d65c2216aad8af8585c68da539a8ef71b9c4fadf
Diego Desktop <disouzauva@gmail.com>
2021-02-10 22:31:13 -0300

    Adicionado a função de criar Círculo e a função de draw

--- app/script.js
+++ app/script.js
@@ -12,3 +12,23 @@ function criaRetangulo(cor,xInicial,yInicial,xFinal,yFinal) {
     brush.fillStyle = cor;
     brush.fillRect(xInicial,yInicial,xFinal,yFinal);
 }
+
+function criaBola(cor,x,y,raio) {
+    brush.fillStyle = cor;
+    brush.beginPath();
+    brush.arc(x,y,raio,0,2 * Math.PI);
+    brush.fill();
+}
+
+
+
+
+
+
+// Função Atualiza
+
+function draw() {
+
+}
+
+setInterval(draw, 20);
